b010227f5bc6a7e9c047bd2dd669536141673f47
orlandouchiha425 <valadez425@gmail.com>
2022-04-19 18:43:10 -0400

    recreating to deploy separately

---
+++ server.js
@@ -0,0 +1,15 @@
+
+require('dotenv').config();
+const express= require('express')
+const app=express()
+const PORT = process.env.PORT || 3000;
+const toDoController=require('./controllers/todolist')
+const cors=require('cors')
+
+//MiddleWare
+app.use(cors())
+
+app.use(express.json())
+app.use('/todo',toDoController)
+
+app.listen(PORT,()=>{console.log(`i am connected to ${PORT}`)})
